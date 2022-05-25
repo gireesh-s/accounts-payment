4da8f712334a8095dabb5b152a3c89e3e6b34bf7
gireesh-s <gireesh4615@gmail.com>
2022-05-25 11:52:35 +0530

    Completed modal and adding new card

--- src/Page/PaymentInfo/PaymentInfo.js
+++ src/Page/PaymentInfo/PaymentInfo.js
@@ -24,6 +24,11 @@ const PaymentInfo = () => {
 
   const [defaultPymnt, setDefaultPymnt] = useState("**************6312");
   const [open, setOpen] = useState(false)
+  const [values, setValues] = useState({
+      cardType: "", cardNumber: "", firstName: "", lastName: "", expDate: ""
+  })
+
+  const { cardType, cardNumber, firstName, lastName, expDate } = values;
 
   const handleOpen = () => {
       setOpen(true)
@@ -31,6 +36,13 @@ const PaymentInfo = () => {
 
   const handleClose = () => {
       setOpen(false)
+  }
+
+  const handleSubmit = (event) => {
+      event.preventDefault();
+      console.log(values);
+      paymentMethods.push(values);
+      handleClose();
   }
 
   return (
@@ -54,17 +66,134 @@ const PaymentInfo = () => {
                         <Divider style={{margin:"10px 0"}}/>
                         <div style={{width:"100%"}}>
                             <div className='modal-input-no'>
-                                <div>Card Number*</div>
+                                <div style={{display:"flex"}}>
+                                    <div style={{flexGrow:"1"}}>Card Number*</div>
+                                    <img
+                                     src={Amex} 
+                                     alt="card" 
+                                     className={ values.cardType === "Amex" ? "modal-card-selected" : "modal-card-unselected" }
+                                     onClick={()=>setValues({
+                                         ...values,
+                                         cardType: "Amex"
+                                     })}
+                                    />
+                                    <img
+                                     src={Discover} 
+                                     alt="card" 
+                                     className={ values.cardType === "Discover" ? "modal-card-selected" : "modal-card-unselected" }
+                                     onClick={()=>setValues({
+                                         ...values,
+                                         cardType: "Discover"
+                                     })}
+                                    />
+                                    <img
+                                     src={Mastercard} 
+                                     alt="card" 
+                                     className={ values.cardType === "Mastercard" ? "modal-card-selected" : "modal-card-unselected" }
+                                     onClick={()=>setValues({
+                                         ...values,
+                                         cardType: "Mastercard"
+                                     })}
+                                    />
+                                    <img
+                                     src={Visa} 
+                                     alt="card" 
+                                     className={ values.cardType === "Visa" ? "modal-card-selected" : "modal-card-unselected" }
+                                     onClick={()=>setValues({
+                                         ...values,
+                                         cardType: "Visa"
+                                     })}
+                                    />
+                                </div>
                                 <div style={{marginTop:"13px"}}>
-                                    <input type="text" style={{border:"1px solid #C8B9AB", borderRadius:"5px", height:"35px", width:"100%"}}/>
+                                    <input
+                                     type="text"
+                                     style={{border:"1px solid #C8B9AB", borderRadius:"5px", height:"35px", width:"100%"}}
+                                     value={cardNumber}
+                                     onChange={(e)=>setValues({
+                                         ...values,
+                                         cardNumber: e.target.value
+                                     })}
+                                    />
                                 </div>
                             </div>
-                            <div className='modal-input-dt'>
-                                <div>Expirary Date*</div>
-                                <div style={{marginTop:"13px"}}>
-                                    <input type="text" style={{border:"1px solid #C8B9AB", borderRadius:"5px", height:"35px", width:"100%"}}/>
+                            <div className='modal-dt-cvv-con'>
+                                <div className='modal-input-dt'>
+                                    <div>Expirary Date*</div>
+                                    <div style={{marginTop:"13px"}}>
+                                        <input
+                                         type="text" 
+                                         style={{border:"1px solid #C8B9AB", borderRadius:"5px", height:"35px", width:"100%"}}
+                                         value={expDate}
+                                         onChange={(e)=>setValues({
+                                            ...values,
+                                            expDate: e.target.value
+                                         })}
+                                        />
+                                    </div>
+                                </div>
+                                &nbsp;
+                                &nbsp;
+                                &nbsp;
+                                &nbsp;
+                                <div className='modal-input-cvv'>
+                                    <div>CVV*</div>
+                                    <div style={{marginTop:"13px"}}>
+                                        <input type="password" style={{border:"1px solid #C8B9AB", borderRadius:"5px", height:"35px", width:"100%"}}/>
+                                    </div>
                                 </div>
                             </div>
+                            <div className='modal-input-name'>
+                                <div>Name on Card*</div>
+                                <div style={{marginTop:"13px", display:"flex"}}>
+                                    <input
+                                     type="text" 
+                                     placeholder='First Name' 
+                                     style={{border:"1px solid #C8B9AB", borderRadius:"5px", height:"35px", width:"100%"}}
+                                     value={firstName}
+                                     onChange={(e)=>setValues({
+                                        ...values,
+                                        firstName: e.target.value
+                                     })}
+                                    />
+                                    &nbsp;
+                                    &nbsp;
+                                    &nbsp;
+                                    &nbsp;
+                                    <input
+                                     type="text" 
+                                     placeholder='Last Name' 
+                                     style={{border:"1px solid #C8B9AB", borderRadius:"5px", height:"35px", width:"100%"}}
+                                     value={lastName}
+                                     onChange={(e)=>setValues({
+                                        ...values,
+                                        lastName: e.target.value
+                                     })}
+                                    />
+                                </div>
+                            </div>
+                            <div className='modal-input-name'>
+                                <input type="checkbox"/>
+                                &nbsp;
+                                <label>Make this my default credit card</label>
+                            </div>
+                        </div>
+                        <Divider style={{padding:"20px 0", margin:"15px 0"}}/>
+                        <div style={{float:"right"}}>
+                            <Button
+                             variant='outlined' 
+                             size='small' 
+                             style={{textTransform:"none", borderColor:"#2D5566", color:"#2D5566"}}
+                              onClick={handleClose}
+                            >Cancel</Button>
+                            &nbsp;
+                            &nbsp;
+                            <Button
+                             variant='contained' 
+                             size='small' 
+                             style={{textTransform:"none", backgroundColor:"#2D5566"}}
+                             onClick={handleSubmit}
+                            >Add Card</Button>
                         </div>
                     </Box>
                 </Modal>
@@ -101,7 +230,7 @@ const PaymentInfo = () => {
                                 <div className='card-content'>
                                     <Typography style={{textAlign:"right",fontSize:"13px"}}>{data.cardType}</Typography>
                                     <Typography style={{textAlign:"right",fontSize:"13px"}}>{data.cardNumber}</Typography>
-                                    <Typography style={{textAlign:"right",fontSize:"13px"}}>{data.nameOnCard}</Typography>
+                                    <Typography style={{textAlign:"right",fontSize:"13px"}}>{data.firstName}&nbsp;{data.lastName}</Typography>
                                     <Typography style={{textAlign:"right",fontSize:"13px"}}>{data.expDate}</Typography>
                                 </div>
                             </div>
@@ -113,9 +242,9 @@ const PaymentInfo = () => {
                                     <div>Default</div>
                                 </div>
                                 <div style={{display:"flex"}}>
-                                    <Link href="#" className='card-link'>Edit</Link>
+                                    <Link to="#" className='card-link'>Edit</Link>
                                     <div className='card-link'>|</div>
-                                    <Link href="#" className='card-link'>Remove</Link>
+                                    <Link to="#" className='card-link'>Remove</Link>
                                 </div>
                             </div>
                         </Grid>
@@ -157,7 +286,7 @@ const PaymentInfo = () => {
                                 <div className='card-content'>
                                     <Typography style={{textAlign:"right",fontSize:"13px"}}>{data.cardType}</Typography>
                                     <Typography style={{textAlign:"right",fontSize:"13px"}}>{data.cardNumber}</Typography>
-                                    <Typography style={{textAlign:"right",fontSize:"13px"}}>{data.nameOnCard}</Typography>
+                                    <Typography style={{textAlign:"right",fontSize:"13px"}}>{data.firstName}&nbsp;{data.lastName}</Typography>
                                     <Typography style={{textAlign:"right",fontSize:"13px"}}>{data.expDate}</Typography>
                                 </div>
                             </div>
@@ -172,9 +301,9 @@ const PaymentInfo = () => {
                                     <div>Make Default</div>
                                 </div>
                                 <div style={{display:"flex"}}>
-                                    <Link href="#" className='card-link'>Edit</Link>
+                                    <Link to="#" className='card-link'>Edit</Link>
                                     <div className='card-link'>|</div>
-                                    <Link href="#" className='card-link'>Remove</Link>
+                                    <Link to="#" className='card-link'>Remove</Link>
                                 </div>
                             </div>
                         </Grid>
